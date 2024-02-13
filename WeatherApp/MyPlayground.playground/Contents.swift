3a16edc3d7d1f0b7e04fd90a030e6d40acfc8824
constdreamcoder <scdreamricher@gmail.com>
2024-02-14 01:46:04 +0900

    플레이그라운드 삭제

--- WeatherApp/MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-var greeting = "Hello, playground"
-
-var temp = Date(timeIntervalSince1970: 1707836400)
-let dateFormatter = DateFormatter()
-dateFormatter.dateFormat = "yyyy.MM.dd hh.mm.ss"
-dateFormatter.locale = Locale(identifier: "ko-KR")
-dateFormatter.string(from: temp)
